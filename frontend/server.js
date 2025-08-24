d685ea642527e72d307d8ffcac6ece025fbf6393
Goshenkata <goshenkataklev@gmail.com>
2025-08-24 16:46:36 +0300

    remove client: |

--- frontend/server.js
+++ frontend/server.js
@@ -4,19 +4,6 @@ const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-let client;
-// Initialize OpenID Client
-async function initializeClient() {
-    const issuer = await Issuer.discover('https://cognito-idp.eu-central-1.amazonaws.com/eu-central-1_VG1OzzrL0');
-    client = new issuer.Client({
-        client_id: '6psh5d9pah9j64kr7v5cdqfa2j',
-        client_secret: '<client secret>',
-        redirect_uris: ['https://goshenkata-frontend.s3.eu-central-1.amazonaws.com/index.html'],
-        response_types: ['code']
-    });
-};
-initializeClient().catch(console.error);
-
 // Serve static files from the static directory
 app.use(express.static(path.join(__dirname, 'static')));
 
